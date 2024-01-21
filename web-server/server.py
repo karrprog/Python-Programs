86fbd838058bc4d326916c40c21920fbbf4ca430
Nathaniel Karr <41494597+karrprog@users.noreply.github.com>
2024-01-20 21:05:46 -0600

    WebServerDev1

---
+++ web-server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('./index.html')
+
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on blogs!'
